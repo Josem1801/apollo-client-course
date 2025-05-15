680f121c82b1fb0e9c7bc73ebbfd378c4c224c76
joserg1801 <jose.rosales@hostpal.mx>
2025-05-14 20:52:56 -0600

    chore: integrate apollo client

--- src/pages/tracks.tsx
+++ src/pages/tracks.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout } from "../components";
+import Track from "./track";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  return <Layout grid></Layout>;
 };
 
 export default Tracks;
